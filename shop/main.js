948042d34808c0163769adad8b06efa33babe0a8
WelkerAcero <universityestudiosca@gmail.com>
2022-07-08 20:34:57 -0500

    Almost ready - the team has worked by Live Share

--- shop/main.js
+++ shop/main.js
@@ -1,25 +1,65 @@
 import '../style.css'
 import './style.css'
+
 const Url = new URL(window.location)
 const urlParams = new URLSearchParams(Url.searchParams);
+const app = document.getElementById('app');
+//paginacion
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+const key = 'd2b1df9d64af7fb2a0342bd9d23e1449'
 
-const app = document.querySelector('#app');
+btnSiguiente.addEventListener('click', () => {
+	if(pagina < 1000){
+		pagina += 1;
+		cargarPeliculas();
+	}
+});
+
+btnAnterior.addEventListener('click', () => {
+	if(pagina > 1){
+		pagina -= 1;
+		cargarPeliculas();
+	}
+});
+
+const cargarPeliculas = async() => {
+	try {
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=es-MX&page=${pagina}`);
+	
+		console.log(respuesta);
+
+		// Si la respuesta es correcta
+		if(respuesta.status === 200){
+			const datos = await respuesta.json();
+			
+			let peliculas = '';
+			datos.results.forEach(pelicula => {
+				peliculas += `
+					<div class="pelicula">
+						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+					</div>
+				`;
+			});
+
+			app.innerHTML = peliculas;
+
+		} else if(respuesta.status === 401){
+			console.log('key incorrecta');
+		} else if(respuesta.status === 404){
+			console.log('La pelicula que buscas no existe');
+		} else {
+			console.log('Hubo un error y no sabemos que paso');
+		}
+
+	} catch(error){
+		console.log(error);
+	}
+
+}
+
+cargarPeliculas();
 
 
-const template = `
-<h1>Hello world! Shop Page</h1>
-${urlParams.get("product")}
-<button id="accion"> Cambio </button>
-<a href="/index.html">Home</a>
-`;
-app.innerHTML = template
 
-const button = document.getElementById('accion');
-button.addEventListener("click",() =>{
-    const h1 = document.querySelector("h1")
-    console.log("click", h1.style.color)
-    if(h1.style.color === "blue")
-        h1.style.color = "red";
-    else
-        h1.style.color = "blue";
-})
