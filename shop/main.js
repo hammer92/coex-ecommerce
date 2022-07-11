0a11ff1f87d402881308f1bc4a22d9396d6cae1b
AndresVargasA <afva2710@gmail.com>
2022-07-11 17:43:00 -0500

    Añadiendo footer cards

--- shop/main.js
+++ shop/main.js
@@ -1,10 +1,11 @@
 import '../style.css'
 import './style.css'
 
+
 const Url = new URL(window.location)
 const urlParams = new URLSearchParams(Url.searchParams);
-const app = document.getElementById('app');
-const filtros = document.getElementById('filtros');
+const app = document.querySelector('#app');
+
 //paginacion
 let pagina = 1;
 const btnAnterior = document.getElementById('btnAnterior');
@@ -26,56 +27,53 @@ btnAnterior.addEventListener('click', () => {
 	}
 });
 
-const cargarFiltros = async () => {
-	try {
-		let filt = '';
-
-		filt += `	
-	<form id="form" class="form">	
-		<input type="text" class="search" id="search" name="gsearch" placeholder="Buscar">
-    	<p class="order">Order:</p>
-    <select class="list-order" name="" id="">
-      <ul>
-        <li class="list-order">
-          <option value="">Mas reciente</option>
-        </li>
-        <li class="list-order">
-          <option value="">Mas vistas</option>
-        </li>
-        <li class="list-order">
-          <option value="">Mas populares</option>
-        </li>
-      </ul>
-    </select>
-	</form>
-	`;
-	filtros.innerHTML = filt;
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-
 
 const cargarPeliculas = async () => {
 	try {
 		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=es-MX&page=${pagina}`);
+		const respuestaGeneros = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`);
 
 		console.log(respuesta);
 
 		// Respuesta OK
-		if (respuesta.status === 200) {
-			//accedemos a los datos 
+		if (respuesta.status === 200 && respuestaGeneros.status === 200) {
+			//accedemos a los datos
 			const datos = await respuesta.json();
+      		const generos = await respuestaGeneros.json();
 
 			let peliculas = '';
 			datos.results.forEach(pelicula => {
-				peliculas += `
-				
-					<div class="pelicula">
-					<span class="circulo">${pelicula.vote_average}</span>	
-					<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">	
+        if(pelicula.overview === ""){
+          	pelicula.overview = "Dont have overview"
+        }
+        let generoId = pelicula.genre_ids[0];
+        let generoName = "";
+        generos.genres.forEach(genero =>{
+          	if(genero.id === generoId){
+            	generoName = genero.name;
+          	}
+        })
+
+		peliculas += `
+			<div class="pelicula">
+            <div class="arr">
+            <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                <!-- <p class="title_poster">${pelicula.original_title}</p>--!>
+            </div>
+            <div class="aba">
+                <div class="info">
+					<div>
+						${generoName}
 					</div>
+					<div>
+						<span class="circulo">${pelicula.vote_average}</span>
+					</div>
+					</div>
+					<div>
+						<button class ="carrito" id="carrito">Add to card</button>
+					</div>
+            	</div>
+			</div>
 				`;
 			});
 
@@ -95,8 +93,4 @@ const cargarPeliculas = async () => {
 
 }
 
-cargarFiltros();
 cargarPeliculas();
-
-
-
