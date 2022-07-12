eea6cc83c1faea1273617fb8eaeb1df107280b92
simonhl14 <simonh419@gmail.com>
2022-07-12 17:33:56 -0500

    funcion search arreglado

--- shop/main.js
+++ shop/main.js
@@ -118,20 +118,54 @@ function getMovies(url) {
 	})
 }
 
-function showMovies(data) {
+const showMovies = async(data) => {
+	try {
+		const respuestaGeneros = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`);
 
-	let peliculas = '';
-	data.forEach(pelicula => {
+			//accedemos a los datos
+      		const generos = await respuestaGeneros.json();
+
+			let peliculas = '';
+			data.forEach(pelicula => {
+        if(pelicula.overview === ""){
+          	pelicula.overview = "Dont have overview"
+        }
+        let generoId = pelicula.genre_ids[0];
+        let generoName = "";
+        generos.genres.forEach(genero =>{
+          	if(genero.id === generoId){
+            	generoName = genero.name;
+          	}
+        })
+
 		peliculas += `
-				
-					<div class="pelicula">
-					<span class="circulo">${pelicula.vote_average}</span>	
-					<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">	
+			<div class="pelicula">
+            <div class="arr">
+            <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                <!-- <p class="title_poster">${pelicula.original_title}</p>--!>
+            </div>
+            <div class="aba">
+                <div class="info">
+					<div>
+						${generoName}
 					</div>
+					<div>
+						<span class="circulo">${pelicula.vote_average}</span>
+					</div>
+					</div>
+					<div>
+						<button class ="carrito" id="carrito">Add to card</button>
+					</div>
+            	</div>
+			</div>
 				`;
-	});
+			});
 
-	app.innerHTML = peliculas;
+			app.innerHTML = peliculas;
+
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 filtros.addEventListener('submit', (e) => {
