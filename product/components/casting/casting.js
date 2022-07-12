c2299b5cf00d2c2e427327ac6b26f2eb1667b387
jeancagomez17 <jeancagomez94@gmail.com>
2022-07-12 17:30:33 -0500

    iniciando con el controlador

--- product/components/casting/casting.js
+++ product/components/casting/casting.js
@@ -10,6 +10,7 @@ class CarruselCasting {
         <div class="carrusel">
         <div class="slick-list" id="slick-prev">
             <div class="slick-track" id="track">
+
                 <div class="slick">
                     <div>
                     
@@ -17,82 +18,22 @@ class CarruselCasting {
                             <img src="./components/casting/img/image1.webp" alt="Image">
                             <h4> <small> Robert D j </small></h4>
                         </picture>
-
                     </div>
+                    
                 </div>
 
-                <div class="slick">
-                    <div>
-                            
-                            <picture>
-                                <img src="./components/casting/img/image3.webp" alt="Image">
-                                <h4> <small> Actor 4 </small></h4>
-                            </picture>
-                    </div>
-                </div>
-
-                <div class="slick">
-                    <div>
-                            <picture>
-                                <img src="./components/casting/img/image4.jpg" alt="Image">
-                                <h4> <small> Actor 4 </small></h4>
-
-                            </picture>
-                    </div>
-                </div>
-                <div class="slick">
-                    <div>
-                            
-                            <picture>
-                                <img src="./components/casting/img/image5.webp" alt="Image">
-                                <h4> <small> Actor 4 </small></h4>
-
-                            </picture>
-                    </div>
-                </div>
-                <div class="slick">
-                  <div>
-                          
-                          <picture>
-                              <img src="./components/casting/img/image5.webp" alt="Image">
-                              <h4> <small> Actor 4 </small></h4>
-                          </picture>
-                  </div>
-              </div>
-              <div class="slick">
-                <div>
-                        <picture>
-                            <img src="./components/casting/img/image5.webp" alt="Image">
-
-                        </picture>
-                        <h4> <small> Actor 4 </small></h4>
-                </div>
-            </div>
-            <div class="slick">
-              <div>
-                      
-                      <picture>
-                          <img src="./components/casting/img/image5.webp" alt="Image">
-                          <h4> <small> Actor 4 </small></h4>
-
-                      </picture>
-
-              </div>
-          </div>
-
+               
                 
-
-
-
+                
+                
+                
             </div>
             
-
         </div>
         <div role="tabList" class="carrusel-ind"></div>
     </div>
  
   
-
         `
     }
     move(){
--- product/main.js
+++ product/main.js
@@ -1,18 +1,16 @@
 import "../style.css";
 import "./style.css";
-import CartBtn from './components/cart-btn/cart-btn.js'
 import MovieController from "./controllers/movie/movieController";
+import CarruselCasting  from "./components/casting/casting";
+import CarouselRecomended from './components/carousel-recomended/carousel-recomended.js'
+
 const Url = new URL(window.location);
 const urlParams = new URLSearchParams(Url.searchParams);
 
 const app = document.querySelector("#app");
-const template = `
-  <h1>Hello world! Product Page</h1>
-  ${urlParams.get("product")}
-  <button id="accion">Cambio</button>
-  <a href="/index.html">Home</a>
-  <div id='btn-container' ></div>
-`;
+
+const template = `<div id="carousel-recomended"></div>
+<div id="carruselCasting"></div>`;
 app.innerHTML = template;
 const carousel = document.querySelector('#carousel-recomended')
 const carouselInstance = new CarouselRecomended(window)
@@ -29,3 +27,51 @@ casting.move();
 
 
 
+
+
+
+
+const movie = new MovieController(155);
+document.addEventListener('DOMContentLoaded', () =>{
+  const container = document.getElementById('lista');
+  movie.getRecommended().then(value => {
+    console.log(value);
+    value.forEach(e=>{
+      let ejemplo = 
+      `<div class="carousel__elemento">
+          <img class="carousel__img" src="${e.image_link}" alt="${e.name}">
+          <div class="contenedor__elemento">
+              <p class="carousel__titulo" >${e.name}</p>
+          </div>
+      </div>`
+      container.innerHTML += ejemplo
+    })
+  })
+})
+
+
+document.addEventListener('DOMContentLoaded', () =>{
+  const container = document.getElementById('track');
+  movie.getCasting().then(value => {
+    console.log(value);
+
+    value.forEach(e=>{
+      let ejemplo = 
+      
+      `
+      
+  <div class="slick">
+      <div>
+            <picture>
+              <img src="${e.image_link}" alt="Image">
+               <h4> <small> </small></h4>
+          </picture>
+      </div>
+  </div>
+
+
+      `
+      container.innerHTML += ejemplo
+    })
+  })
+})
