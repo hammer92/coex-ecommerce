3d875024c2dc0eea52d9b1e5b39c47e963d0d20d
Yertrix <yersongalvis03@gmail.com>
2022-07-12 23:09:45 -0500

    optimizing redundant code and adding pagination css

--- shop/main.js
+++ shop/main.js
@@ -6,28 +6,19 @@ const Url = new URL(window.location)
 const urlParams = new URLSearchParams(Url.searchParams);
 const filtros = document.getElementById('filtros');
 const app = document.querySelector('#app');
-
-//paginacion
-let pagina = 1;
-const btnAnterior = document.getElementById('btnAnterior');
-const btnSiguiente = document.getElementById('btnSiguiente');
 const key = 'd2b1df9d64af7fb2a0342bd9d23e1449'
 const searchURL = "https://api.themoviedb.org/3/search/movie?api_key=d2b1df9d64af7fb2a0342bd9d23e1449";
 
-//Paginacion
-btnSiguiente.addEventListener('click', () => {
-	if (pagina < 1000) {
-		pagina += 1;
-		cargarPeliculas();
-	}
-});
+//paginacion
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
 
-btnAnterior.addEventListener('click', () => {
-	if (pagina > 1) {
-		pagina -= 1;
-		cargarPeliculas();
-	}
-});
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
 
 const cargarFiltros = async () => {
 	try {
@@ -48,14 +39,14 @@ let categorySelected = document.querySelectorAll("#categorySelected");
 categorySelected.forEach(element => {
 	element.addEventListener("click", async () => {
 
-		categorySelected.forEach(minCategory =>{
-			if(minCategory.getAttribute('value') != element.getAttribute('value')){
+		categorySelected.forEach(minCategory => {
+			if (minCategory.getAttribute('value') != element.getAttribute('value')) {
 				minCategory.classList.remove("seleccionado");
 			}
 		})
 
 		element.classList.add("seleccionado");
-		getByCategory = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&page=${pagina}&with_genres=${element.value}`;
+		getByCategory = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&with_genres=${element.value}`;
 		console.log("Seleccionaste: " + element.value);
 		cargarPeliculas(getByCategory);
 	});
@@ -63,10 +54,9 @@ categorySelected.forEach(element => {
 
 
 const cargarPeliculas = async (category) => {
-
-	let allMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=es-MX&page=${pagina}`;
-	let generos = `https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`;
-
+	lastUrl = category;
+	let allMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`;
+	
 	if (category == "") {
 		console.log("No hay categoría seleccionada");
 	} else {
@@ -76,99 +66,63 @@ const cargarPeliculas = async (category) => {
 	console.log(`Has seleccionado categorías en URL: ${allMovies}`);
 
 	try {
-		const respuesta = await fetch(allMovies);
-		const respuestaGeneros = await fetch(generos);
-
-		console.log(respuesta);
-
-		// Respuesta OK
-		if (respuesta.status === 200 && respuestaGeneros.status === 200) {
-			//accedemos a los datos
-			const datos = await respuesta.json();
-			const generos = await respuestaGeneros.json();
-
-			let peliculas = '';
-			datos.results.forEach(pelicula => {
-				if (pelicula.overview === "") {
-					pelicula.overview = "Dont have overview"
-				}
-				let generoId = pelicula.genre_ids[0];
-				let generoName = "";
-				generos.genres.forEach(genero => {
-					if (genero.id === generoId) {
-						generoName = genero.name;
-					}
-				})
-
-				peliculas += `
-			<div class="pelicula">
-            <div class="arr">
-            <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-                <!-- <p class="title_poster">${pelicula.original_title}</p>--!>
-            </div>
-            <div class="aba">
-                <div class="info">
-					<div>
-						${generoName}
-					</div>
-					<div>
-						<span class="circulo">${pelicula.vote_average}</span>
-					</div>
-					</div>
-					<div>
-						<button class ="carrito" id="carrito">Add to card</button>
-					</div>
-            	</div>
-			</div>
-				`;
-			});
-
-			app.innerHTML = peliculas;
-
-		} else if (respuesta.status === 401) {
-			console.log('key incorrecta');
-		} else if (respuesta.status === 404) {
-			console.log('ERROR 404: Movie doesnt exist in our records');
-		} else {
-			console.log('Error');
-		}
-
+		getMovies(allMovies);
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-function getMovies(url) {
 
+function getMovies(url) {
+	lastUrl = url;
 	fetch(url).then(res => res.json()).then(data => {
 		console.log(data.results)
 		if (data.results.length !== 0) {
 			showMovies(data.results);
+			currentPage = data.page;
+			nextPage = currentPage + 1;
+			prevPage = currentPage - 1;
+			totalPages = data.total_pages;
+
+			current.innerText = currentPage;
+
+			if (currentPage <= 1) {
+				prev.classList.add('disabled');
+				next.classList.remove('disabled')
+			} else if (currentPage >= totalPages) {
+				prev.classList.remove('disabled');
+				next.classList.add('disabled')
+			} else {
+				prev.classList.remove('disabled');
+				next.classList.remove('disabled')
+			}
+		} else {
+			app.innerHTML = `<h1 class="no-results">No Results Found</h1>`
 		}
 	})
 }
 
-const showMovies = async(data) => {
+const showMovies = async (data) => {
 	try {
 		const respuestaGeneros = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`);
 
-			//accedemos a los datos
-      		const generos = await respuestaGeneros.json();
+		//accedemos a los datos
+		const generos = await respuestaGeneros.json();
 
-			let peliculas = '';
-			data.forEach(pelicula => {
-        if(pelicula.overview === ""){
-          	pelicula.overview = "Dont have overview"
-        }
-        let generoId = pelicula.genre_ids[0];
-        let generoName = "";
-        generos.genres.forEach(genero =>{
-          	if(genero.id === generoId){
-            	generoName = genero.name;
-          	}
-        })
+		let peliculas = '';
+		data.forEach(pelicula => {
+			if (pelicula.overview === "") {
+				pelicula.overview = "Dont have overview"
+			}
+			let generoId = pelicula.genre_ids[0];
+			let generoName = "";
+			generos.genres.forEach(genero => {
+				if (genero.id === generoId) {
+					generoName = genero.name;
+				}
+			})
 
-		peliculas += `
+			peliculas += `
 			<div class="pelicula">
             <div class="arr">
             <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
@@ -189,9 +143,9 @@ const showMovies = async(data) => {
             	</div>
 			</div>
 				`;
-			});
+		});
 
-			app.innerHTML = peliculas;
+		app.innerHTML = peliculas;
 
 	} catch (error) {
 		console.log(error);
@@ -211,6 +165,36 @@ filtros.addEventListener('submit', (e) => {
 	}
 
 });
+
+//Paginacion
+current.addEventListener('click', () => {
+	if (prevPage > 0) {
+		pageCall(prevPage);
+	}
+})
+
+next.addEventListener('click', () => {
+	if (nextPage <= totalPages) {
+		pageCall(nextPage);
+	}
+})
+
+function pageCall(page) {
+	let urlSplit = lastUrl.split('?');
+	let queryParams = urlSplit[1].split('&');
+	let key = queryParams[queryParams.length - 1].split('=');
+	if (key[0] != 'page') {
+		let url = lastUrl + '&page=' + page
+		getMovies(url);
+	} else {
+		key[1] = page.toString();
+		let a = key.join('=');
+		queryParams[queryParams.length - 1] = a;
+		let b = queryParams.join('&');
+		let url = urlSplit[0] + '?' + b
+		getMovies(url);
+	}
+}
 
 cargarFiltros();
 cargarPeliculas(getByCategory);
