71ebf578f0eadc0ac6c7e7b70e13b6fa17ce72a9
Julian Garcia <jcgrrincon@gmail.com>
2022-07-15 18:00:31 -0500

    correcion de errores al hacer merging

--- cart/main.js
+++ cart/main.js
@@ -15,34 +15,10 @@ const user = new Object();
 user.email = 'admin@admin.com';
 user.password = 'admin';
 
-<<<<<<< HEAD
-
-function Log() {
-	if (sesion) {
-		const SIGIN = document.getElementById('sigin');
-		if (sesion === 'false') {
-			SIGIN.innerHTML = 'Sig In';
-		} else if (sesion === 'true') {
-			SIGIN.innerHTML = user.email;
-		}
-	} else {
-		localStorage.setItem('iniciosesion', false);
-	}
-}
-
-
-
-
-function login(){
-    var iniciosesion = false;
-    localStorage.setItem('iniciosesion', iniciosesion);
-    app.innerHTML = templatelogin;
-=======
 function Login() {
 	app.innerHTML = templatelogin;
 	var iniciosesion = false;
 	localStorage.setItem('iniciosesion', iniciosesion);
->>>>>>> c682f37ce53f51351c48fd72e320750929d34641
 	const form = document.getElementById('form_login');
 	form.onsubmit = () => {
 		const mail = document.getElementById('email');
@@ -57,12 +33,8 @@ function Login() {
 			if (correo == user.email && contra == user.password) {
                 iniciosesion = true
 				localStorage.setItem('iniciosesion', iniciosesion);
-<<<<<<< HEAD
                 alert('inicio de sesion correcto');
 				window.location.href ='../shop/index.html';
-=======
-				// app.innerHTML = templateprueba;
->>>>>>> c682f37ce53f51351c48fd72e320750929d34641
 			} else {
 				alert('Credenciales invalidas');
 			}
@@ -121,17 +93,6 @@ function renderHistory() {
 	document.addEventListener("load", readMovieList());
 }
 
-const verifyRender = ()=>{
-    console.log(localStorage.getItem('iniciosesion'))
-    if (localStorage.getItem('iniciosesion') == true) {
-        renderHistory();
-    } else {
-        login();
-    }
-}
-
-document.addEventListener('load', verifyRender())
-
 const template = `
 <h1>Hello world! Product Page</h1>
 ${urlParams.get('product')}
@@ -142,8 +103,6 @@ ${urlParams.get('product')}
 <button id="myorder">myOrder</button>
 `;
 
-<<<<<<< HEAD
-=======
 if (sesion){
 	console.log('sesion: ', sesion)
 	if (sesion === 'false') {
@@ -159,7 +118,6 @@ if (sesion){
 	localStorage.setItem('iniciosesion', false);
 	Login();
 }
->>>>>>> c682f37ce53f51351c48fd72e320750929d34641
 
 // app.innerHTML = templateHistory;
 // app.innerHTML = template;
