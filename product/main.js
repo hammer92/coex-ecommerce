5f651e3a2bde9a14e192bb24d3b09c01fee0dfaf
Juan Pablo Bautista Cala <cala.js@icloud.com>
2022-07-15 02:52:13 -0500

    minor chages

--- product/main.js
+++ product/main.js
@@ -8,6 +8,8 @@ const urlParams = new URLSearchParams(Url.searchParams);
 const id = urlParams.get("product")
 
 const app = document.querySelector('#app');
+const testID = 550
+const section = new MovieSection(testID);
 
 const template =`
 <h1>Hello world! Cart Page</h1>
@@ -16,21 +18,7 @@ const template =`
 `
 //app.innerHTML = template
 
-const button = document.getElementById('accion');
-button.addEventListener("click",() =>{
-    const h1 = document.querySelector("h1")
-    console.log("click", h1.style.color)
-    if(h1.style.color === "blue")
-        h1.style.color = "red";
-    else
-        h1.style.color = "blue";
-})
-import "../style.css";
-import "./style.css";
 
-const testID = 550
-
-const section = new MovieSection(testID);
 
 section.template()
 .then( template => {
