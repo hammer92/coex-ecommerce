2cac9433a4eac8f17f9ae800c55cd098c4313c7b
Jerson Guerrero <yesidlds54@gmail.com>
2022-07-19 10:21:25 -0500

    primeros pasos en la refacturación de las clases de Shop

--- shop/main.js
+++ shop/main.js
@@ -1,37 +1,37 @@
-import 'normalize.css'
-import '../cart/style-cart.css'
-import '../style.css'
-import './style.css'
+import 'normalize.css';
+import '../cart/style-cart.css';
+import '../style.css';
+import './style.css';
 import '../cart/component/starsComponent.js';
 import templatelogin from '../cart/views/login.js';
 
 import { addMovieList, getData } from '../cart/lib/conection.js';
 
-const Url = new URL(window.location)
+const Url = new URL(window.location);
 const urlParams = new URLSearchParams(Url.searchParams);
 const filtros = document.getElementById('filtros');
 const app = document.querySelector('#app');
-const key = 'd2b1df9d64af7fb2a0342bd9d23e1449'
-const searchURL = "https://api.themoviedb.org/3/search/movie?api_key=d2b1df9d64af7fb2a0342bd9d23e1449";
+const key = 'd2b1df9d64af7fb2a0342bd9d23e1449';
+const searchURL =
+	'https://api.themoviedb.org/3/search/movie?api_key=d2b1df9d64af7fb2a0342bd9d23e1449';
 
-const historialBtn =document.getElementById('historial-btn') 
+const historialBtn = document.getElementById('historial-btn');
 
-
-document.getElementById('checkButton').addEventListener('click', ()=>{
+document.getElementById('checkButton').addEventListener('click', () => {
 	let data = getData();
 	addMovieList(data);
 	localStorage.setItem('shoppingCart', []);
 	localStorage.setItem('statusback', 'cart');
-	window.location = '../cart/index.html'
+	window.location = '../cart/index.html';
 });
 
-document.getElementById('history-shop').addEventListener('click', ()=>{
+document.getElementById('history-shop').addEventListener('click', () => {
 	localStorage.setItem('statusback', 'cart');
-})
+});
 //paginacion
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const current = document.getElementById('current')
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const current = document.getElementById('current');
 
 let currentPage = 1;
 let nextPage = 2;
@@ -53,38 +53,39 @@ const cargarFiltros = async () => {
 	} catch (error) {
 		console.log(error);
 	}
-}
+};
 
-let getByCategory = "";
-let categorySelected = document.querySelectorAll("#categorySelected");
+let getByCategory = '';
+let categorySelected = document.querySelectorAll('#categorySelected');
 
-categorySelected.forEach(element => {
-	element.addEventListener("click", async () => {
-
-		categorySelected.forEach(minCategory => {
-			if (minCategory.getAttribute('value') != element.getAttribute('value')) {
-				minCategory.classList.remove("seleccionado");
+categorySelected.forEach((element) => {
+	element.addEventListener('click', async () => {
+		categorySelected.forEach((minCategory) => {
+			if (
+				minCategory.getAttribute('value') !=
+				element.getAttribute('value')
+			) {
+				minCategory.classList.remove('seleccionado');
 			}
-		})
+		});
 
-		element.classList.add("seleccionado");
+		element.classList.add('seleccionado');
 		getByCategory = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&with_genres=${element.value}`;
-		console.log("Seleccionaste: " + element.value);
+		console.log('Seleccionaste: ' + element.value);
 		cargarPeliculas(getByCategory);
 	});
 });
-
 
 const cargarPeliculas = async (category) => {
 	lastUrl = category;
 	let allMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`;
 
-	if (category == "") {
-		console.log("No hay categoría seleccionada");
+	if (category == '') {
+		console.log('No hay categoría seleccionada');
 	} else {
 		allMovies = category;
 	}
-	
+
 	console.log(`Has seleccionado categorías en URL: ${allMovies}`);
 
 	try {
@@ -92,56 +93,60 @@ const cargarPeliculas = async (category) => {
 	} catch (error) {
 		console.log(error);
 	}
-}
+};
 
 function getMovies(url) {
 	lastUrl = url;
-	fetch(url).then(res => res.json()).then(data => {
-		console.log(data.results)
-		if (data.results.length !== 0) {
-			showMovies(data.results);
-			currentPage = data.page;
-			nextPage = currentPage + 1;
-			prevPage = currentPage - 1;
-			totalPages = data.total_pages;
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data.results);
+			if (data.results.length !== 0) {
+				showMovies(data.results);
+				currentPage = data.page;
+				nextPage = currentPage + 1;
+				prevPage = currentPage - 1;
+				totalPages = data.total_pages;
 
-			current.innerText = currentPage;
+				current.innerText = currentPage;
 
-			if (currentPage <= 1) {
-				prev.classList.add('disabled');
-				next.classList.remove('disabled')
-			} else if (currentPage >= totalPages) {
-				prev.classList.remove('disabled');
-				next.classList.add('disabled')
+				if (currentPage <= 1) {
+					prev.classList.add('disabled');
+					next.classList.remove('disabled');
+				} else if (currentPage >= totalPages) {
+					prev.classList.remove('disabled');
+					next.classList.add('disabled');
+				} else {
+					prev.classList.remove('disabled');
+					next.classList.remove('disabled');
+				}
 			} else {
-				prev.classList.remove('disabled');
-				next.classList.remove('disabled')
+				app.innerHTML = `<h1 class="no-results">No Results Found</h1>`;
 			}
-		} else {
-			app.innerHTML = `<h1 class="no-results">No Results Found</h1>`
-		}
-	})
+		});
 }
 
 const showMovies = async (data) => {
 	try {
-		const respuestaGeneros = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`);
+		const respuestaGeneros = await fetch(
+			`https://api.themoviedb.org/3/genre/movie/list?api_key=${key}`
+		);
 
 		//accedemos a los datos
 		const generos = await respuestaGeneros.json();
 
 		let peliculas = '';
-		data.forEach(pelicula => {
-			if (pelicula.overview === "") {
-				pelicula.overview = "Dont have overview"
+		data.forEach((pelicula) => {
+			if (pelicula.overview === '') {
+				pelicula.overview = 'Dont have overview';
 			}
 			let generoId = pelicula.genre_ids[0];
-			let generoName = "";
-			generos.genres.forEach(genero => {
+			let generoName = '';
+			generos.genres.forEach((genero) => {
 				if (genero.id === generoId) {
 					generoName = genero.name;
 				}
-			})
+			});
 
 			peliculas += `
 			<div class="pelicula">
@@ -167,13 +172,12 @@ const showMovies = async (data) => {
 			</div>
 				`;
 		});
-		
+
 		app.innerHTML = peliculas;
-		
 	} catch (error) {
 		console.log(error);
 	}
-}
+};
 
 filtros.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -186,7 +190,6 @@ filtros.addEventListener('submit', (e) => {
 	} else {
 		cargarPeliculas();
 	}
-
 });
 
 //Paginacion
@@ -194,27 +197,27 @@ prev.addEventListener('click', () => {
 	if (prevPage > 0) {
 		pageCall(prevPage);
 	}
-})
+});
 
 next.addEventListener('click', () => {
 	if (nextPage <= totalPages) {
 		pageCall(nextPage);
 	}
-})
+});
 
 function pageCall(page) {
 	let urlSplit = lastUrl.split('?');
 	let queryParams = urlSplit[1].split('&');
 	let key = queryParams[queryParams.length - 1].split('=');
 	if (key[0] != 'page') {
-		let url = lastUrl + '&page=' + page
+		let url = lastUrl + '&page=' + page;
 		getMovies(url);
 	} else {
 		key[1] = page.toString();
 		let a = key.join('=');
 		queryParams[queryParams.length - 1] = a;
 		let b = queryParams.join('&');
-		let url = urlSplit[0] + '?' + b
+		let url = urlSplit[0] + '?' + b;
 		getMovies(url);
 	}
 }
@@ -223,33 +226,29 @@ cargarFiltros();
 cargarPeliculas(getByCategory);
 
 //--------------------------------------------------
-	// Configuracion del navbar para esta de la sesion
+// Configuracion del navbar para esta de la sesion
 
-	window.onload = ()=>{
+window.onload = () => {
+	const nologinstate = document.querySelector('#btn-nosesion-nav');
+	const loginstate = document.querySelector('#btn-sesion-nav');
+	let estatesesion = localStorage.getItem('iniciosesion');
 
-		const nologinstate = document.querySelector("#btn-nosesion-nav");
-		const loginstate = document.querySelector("#btn-sesion-nav");
-		let estatesesion = localStorage.getItem("iniciosesion");
-	
-		if(estatesesion === 'false'){
-			nologinstate.classList.remove("btn-hidden");
-			loginstate.classList.add("btn-hidden");
-		}else{
-			nologinstate.classList.add("btn-hidden");
-			loginstate.classList.remove("btn-hidden");
-		}
-	
-		
-	};
-	
+	if (estatesesion === 'false') {
+		nologinstate.classList.remove('btn-hidden');
+		loginstate.classList.add('btn-hidden');
+	} else {
+		nologinstate.classList.add('btn-hidden');
+		loginstate.classList.remove('btn-hidden');
+	}
+};
 
-//funcion del boton log out 
+//funcion del boton log out
 const buttonlogout = document.getElementById('logout');
 buttonlogout.addEventListener('click', () => {
-	console.log("click en log out")
+	console.log('click en log out');
 	let estado = false;
 	localStorage.setItem('iniciosesion', estado);
-	window.location.reload()
+	window.location.reload();
 });
 
 //funcion btn log in
@@ -257,7 +256,3 @@ const buttonlogin = document.getElementById('login-btn');
 buttonlogin.addEventListener('click', () => {
 	window.location = '../cart/index.html';
 });
-
-
-
-
