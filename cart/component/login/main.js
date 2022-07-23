843d0c5d91c7766571692b1dceecdb2cb901f409
jeancagomez17 <jeancagomez94@gmail.com>
2022-07-23 00:23:13 -0500

    Terminada refactorizacion de login

--- cart/component/login/main.js
+++ cart/component/login/main.js
@@ -1,12 +1,38 @@
 import './style.css'
 import templateLogin from './index.html?raw';
-
 const IconSrc = '../../../assets/icons';
+const user = new Object();
+user.email = 'admin@admin.com';
+user.password = 'admin';
+
+ function Login() {
+	var iniciosesion = false;
+	localStorage.setItem('iniciosesion', iniciosesion);
+	const form = document.getElementById('form_login');
+	form.onsubmit = () => {
+		const mail = document.getElementById('email');
+		const con = document.getElementById('password');
+		const correo = mail.value;
+		const contra = con.value;
+
+		if (correo == '' || contra == '') {
+			alert('Debe llenar todos los campos');
+		} else {
+			if (correo == user.email && contra == user.password) {
+                iniciosesion = true
+				localStorage.setItem('iniciosesion', iniciosesion);
+                alert('inicio de sesion correcto');
+				window.location ='../shop/index.html';
+			} else {
+				alert('Credenciales invalidas');
+			}
+		}
+	};
+}
 
 export function render(dom){
-    // const app = document.querySelector('#app');
     dom.innerHTML = templateLogin.replace('${IconSrc}', IconSrc);
-    
+    document.addEventListener('load', Login())
 }
 
 
--- cart/main.js
+++ cart/main.js
@@ -1,7 +1,7 @@
 //import '../style.css'
 import './style.css';
 import templateHistory from './views/history.js';
-import templatelogin from './views/login.js';
+import templatelogin from './views/login.js'; 
 import { OrderList } from './views/myorderView.js';
 import './lib/conection.js';
 import { dbConection } from './lib/conection.js';
@@ -128,49 +128,52 @@ function RenderMyOrder(data) {
 // }
 HistoryComponent.render(app,readMovieList());
 
-const Login = ()=> {
-	// app.innerHTML = templatelogin;
-	log.render();
-	// LoginComponent.render(app)
-	var iniciosesion = false;
-	localStorage.setItem('iniciosesion', iniciosesion);
-	const form = document.getElementById('form_login');
-	form.onsubmit = () => {
-		const mail = document.getElementById('email');
-		const con = document.getElementById('password');
-		const correo = mail.value;
-		const contra = con.value;
+// const Login = ()=> {
+// 	// app.innerHTML = templatelogin;
+// 	log.render();
+// 	// LoginComponent.render(app)
+// 	var iniciosesion = false;
+// 	localStorage.setItem('iniciosesion', iniciosesion);
+// 	const form = document.getElementById('form_login');
+// 	form.onsubmit = () => {
+// 		const mail = document.getElementById('email');
+// 		const con = document.getElementById('password');
+// 		const correo = mail.value;
+// 		const contra = con.value;
 
-		if (correo == '' || contra == '') {
-			alert('Debe llenar todos los campos');
-		} else {
-			if (correo == user.email && contra == user.password) {
-                iniciosesion = true
-				localStorage.setItem('iniciosesion', iniciosesion);
-                alert('inicio de sesion correcto');
-				window.location ='../shop/index.html';
-			} else {
-				alert('Credenciales invalidas');
-			}
-		}
-	};
-}
+// 		if (correo == '' || contra == '') {
+// 			alert('Debe llenar todos los campos');
+// 		} else {
+// 			if (correo == user.email && contra == user.password) {
+//                 iniciosesion = true
+// 				localStorage.setItem('iniciosesion', iniciosesion);
+//                 alert('inicio de sesion correcto');
+// 				window.location ='../shop/index.html';
+// 			} else {
+// 				alert('Credenciales invalidas');
+// 			}
+// 		}
+// 	};
+// }
 
-
+ // Refactorizacion de componente login 
 if (sesion){
 	console.log('sesion: ', sesion)
 	if (sesion === 'false') {
-		Login();
+		LoginComponent.render(app);
 	} else {
 		if(localStorage.getItem('statusback', 'cart')){
+			console.log('sesion: ', sesion)
 			HistoryComponent.render(app,readMovieList());
 			// renderHistory();
 		} else {
+			console.log('sesion: ', sesion)
 			window.location = '../shop/index.html';
 		}
 	}
 } else {
+	console.log('sesion: ', sesion)
 	localStorage.setItem('iniciosesion', false);
-	Login();
+	LoginComponent.render(app);
 }
 
